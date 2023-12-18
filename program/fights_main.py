06425c6a74bdee2cc817ee32183d63264057ea0f
Koós Balázs Bence <koosbalazsbence@gmail.com>
2023-12-18 13:27:50 +0100

    asd

--- program/fights_main.py
+++ program/fights_main.py
@@ -1,1 +1,13 @@
-with open('')
+X_szama = Y_szama = W_szama = 0
+with open('../vers/count-x-y-w.txt') as f:
+    for line in f:
+        for karakter in line.strip():
+            if karakter == 'X':
+                X_szama += 1
+            elif karakter == 'Y':
+                Y_szama += 1
+            elif karakter == 'W':
+                W_szama += 1
+            
+eredmeny = X_szama + Y_szama - W_szama
+print(eredmeny)
