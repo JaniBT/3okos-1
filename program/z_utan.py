5a3fb7de3d9030ff289baf13ecc77cc0d427cf0c
JaniBT <janoscsordas072@gmail.com>
2023-12-19 10:06:18 +0100

    Update z_utan.py

--- program/z_utan.py
+++ program/z_utan.py
@@ -1,8 +1,12 @@
 szamok_osszege = 0
 szamok_db = 0
 
-with open('../vers/after-z.txt', 'r', encoding='utf8') as f:
+with open('./vers/after-z.txt', 'r', encoding='utf8') as f:
     for line in f:
-        for karakter in line.strip():
-            if karakter == 'Z':
-                pass
+        for i in range(0, len(line.strip()) - 1):
+            if line[i] == "Z":
+                szamok_osszege += int(line[i+1])
+                szamok_db += 1
+
+eredmeny = szamok_osszege // szamok_db
+print(eredmeny)
