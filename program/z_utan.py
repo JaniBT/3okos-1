8f5377eca241dada70b73bff7a8988588ce61ec1
JaniBT <janoscsordas072@gmail.com>
2023-12-19 10:20:58 +0100

    asd

--- program/z_utan.py
+++ program/z_utan.py
@@ -4,7 +4,7 @@ szamok_db = 0
 with open('./vers/after-z.txt', 'r', encoding='utf8') as f:
     for line in f:
         for i in range(0, len(line.strip()) - 1):
-            if line[i] == "Z":
+            if line[i] == "Z" and line[i + 1].isdigit():
                 szamok_osszege += int(line[i+1])
                 szamok_db += 1
 
