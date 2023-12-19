5e0ea1c3a82cde75f2fc9e788022c1e7ebb72a8e
JaniBT <janoscsordas072@gmail.com>
2023-12-19 10:51:14 +0100

    Create vers.py

---
+++ program/vers.py
@@ -0,0 +1,17 @@
+angol_abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+uj_angol_abc = angol_abc[16:] + angol_abc[:16]
+
+vers = ''
+with open('./vers/caesar.txt', 'r', encoding='utf-8') as f:
+    for line in f:
+        for ch in line:
+            if ch.upper() in angol_abc:
+                index = uj_angol_abc.find(ch.upper())
+                if ch.upper() == ch:
+                    vers += angol_abc[index]
+                else:
+                    vers += angol_abc[index].lower()
+            else:
+                vers += ch
+
+print(vers)
